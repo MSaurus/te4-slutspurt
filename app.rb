934a21d912eeef1e7825e129556e3c421cd499f9
Mike Johansson <mike.johansson.mj@gmail.com>
2019-04-25 14:06:45 +0200

    Added 'pg' gem and fixed the configurations

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require 'sinatra'
+require 'pg'
+require 'yaml'
+
+begin
+  config = YAML.load_file('config/database.yaml')
+  conn = PG.connect host: config['host'],
+                    dbname: config['dbname'],
+                    user: config['user'],
+                    password: config['password']
+  puts conn.server_version
+rescue PG::Error => e
+  puts e.message
+ensure
+  conn.close if conn
+end
 
 get '/' do
   'Hello World'
